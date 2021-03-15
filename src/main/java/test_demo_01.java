e94ca3cbd9b732c90e1faf30f1e7c2b957cdd54e
poooooi <934435147@qq.com>
2021-03-15 22:28:58 +0800

    TestNG init

---
+++ src/main/java/test_demo_01.java
@@ -0,0 +1,22 @@
+/**
+ * @program: TestNG_DEMO
+ * @description:
+ * @author: poooooi
+ * @create: 2021-03-15 15:27
+ **/
+
+import org.testng.annotations.Test;
+
+public class test_demo_01 {
+
+    @Test
+    public void test_01(){
+        System.out.println("HelloWorld");
+    }
+
+    @Test
+    public void test_02(){
+        System.out.println("HelloTestNG");
+    }
+}
+
