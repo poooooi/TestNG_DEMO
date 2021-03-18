c755548aa66a548e8843dd5a508a52848e42f582
poooooi <934435147@qq.com>
2021-03-18 23:14:05 +0800

    实现数据驱动

--- src/main/java/test03_Data/test03_demo_01.java
+++ src/main/java/test03_Data/test03_demo_01.java
@@ -1,5 +1,14 @@
 package test03_Data;
 
+
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.Test;
+import utils.HttpUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @program: TestNG_DEMO
  * @description: 实现数据驱动
@@ -7,8 +16,30 @@ package test03_Data;
  * @create: 2021-03-17 20:37
  **/
 
-public class test03_demo_01 {
-    String URL = "https://accounts.douban.com/j/mobile/login/basic";
 
+public class test03_demo_01 {
+    private String URL = "https://accounts.douban.com/j/mobile/login/basic";
+
+    @Test
+    public void Login() {
+        Map<String, String> postHeader = new HashMap<>();
+        postHeader.put("content-type", "application/json;charset=UTF-8");
+
+        Map<String, String> form = new HashMap<>();
+        form.put("ck", "");
+        form.put("remember", "true");
+        form.put("name", "182XXX1234");
+        form.put("password", "123456");
+
+        String postJson = "{\"ck\":,\"remember\":false,\"name\":\"182XXX1234\",\"password\":\"123456\"}";
+
+        ArrayList<BasicNameValuePair> list = new ArrayList<>();
+        form.forEach((key, value) -> list.add(new BasicNameValuePair(String.valueOf(key), String.valueOf(value))));
+        for (Object object : list) {
+            System.out.print(object);
+        }
+
+        System.out.println("登录结果:"+HttpUtil.doPostByJson(URL,postJson,postHeader));
+    }
 
 }
